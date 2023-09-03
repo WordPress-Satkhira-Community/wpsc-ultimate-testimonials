395c0b939f1f8d9bac7d0a79afeed45b39140df6
wpdelower <delowerjes@gmail.com>
2023-09-03 11:54:04 +0600

    New file uninstall.php added

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @link       https://wpsatkhira.com
+ * @since      1.0
+ *
+ * @package    wps_ultimate_testimonials
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
