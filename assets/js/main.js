4e144e9e70037b84dcf26cf56e089d1543c49a4a
mdkamrulislam0093 <kamrul@kamrul-pc>
2023-09-04 15:04:03 +0600

    asset files added

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+(function($) {
+
+const testimonial = new Swiper('.swiper', {
+	slidesPerView: 1,
+	spaceBetween: 10,	
+	lazy: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},	
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},	
+	breakpoints: {
+		640: {
+		  slidesPerView: 2,
+		  spaceBetween: 30,
+		},
+		1024: {
+		  slidesPerView: 3,
+		  spaceBetween: 30,
+		},	
+	}
+});
+
+
+})(jQuery);
