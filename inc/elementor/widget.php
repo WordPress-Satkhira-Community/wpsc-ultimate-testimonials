4a3e216f03db49de7eb13f5b82239293c9d4de3d
zakirsujon <zakir021063008@gmail.com>
2023-09-12 13:44:25 +0600

    elementor meta data added

--- inc/elementor/widget.php
+++ inc/elementor/widget.php
@@ -1,5 +1,7 @@
 <?php
 
+use \Elementor\Controls_Manager;
+
 class WPSC_Ultimate_Testimonials_Widget extends \Elementor\Widget_Base {
 
 	public function get_name(){
@@ -14,6 +16,21 @@ class WPSC_Ultimate_Testimonials_Widget extends \Elementor\Widget_Base {
 
 	public function get_icon(){
 		return 'eicon-code';
+	}
+
+
+	public function get_custom_help_url() {
+		return 'https://docs.wpsatkhira.com/plugins/wpsc-ultimate-testimonials';
+	}
+
+
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+
+	public function get_keywords() {
+		return [ 'testimonial', 'carousel', 'reviews', 'rating' ];
 	}
 
 
