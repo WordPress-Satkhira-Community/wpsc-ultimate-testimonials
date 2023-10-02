b5d6a32c8987e8094f203a10cb3ad7625f5f7bf5
wpdelower <delowerjes@gmail.com>
2023-10-02 12:16:15 +0600

    Package modified

--- uninstall.php
+++ uninstall.php
@@ -4,7 +4,7 @@
  * @link       https://wpsatkhira.com
  * @since      1.0
  *
- * @package    wps_ultimate_testimonials
+ * @package    wpsc_ultimate_testimonials
  */
 
 // If uninstall not called from WordPress, then exit.
